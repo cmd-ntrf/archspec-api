cabda018ad876951770661f6de4344dd461b57e3
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-02-26 14:55:45 -0500

    Init archspec serverless

---
+++ index.py
@@ -0,0 +1,24 @@
+from archspec.cpu.detect import compatible_microarchitectures
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/<arch>', methods=['POST'])
+def host(arch):
+    """Detects the host micro-architecture and returns it"""
+    error = None
+    if request.method == 'POST':
+        info = {}
+        lines = request.data.decode('utf-8').split('\n')
+        for line in lines:
+            key, separator, value = line.partition(":")
+            if separator != ":" and info:
+                break
+            info[key.strip()] = value.strip()
+        #import pdb; pdb.set_trace()
+        candidates = compatible_microarchitectures(info, arch)
+        target = sorted(
+           candidates, key=lambda t: (len(t.ancestors), len(t.features)), reverse=True
+        )[0]
+        return str(target)
+
